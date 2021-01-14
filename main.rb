bd05f5c91b83bdec29ef2a9737d58e19e83f4f39
Cindy Tham <cindytham@hotmail.com>
2021-01-14 18:37:04 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,30 @@
+
+# 2a Lowercase to Uppercase 
+puts "A. Lowercase to Uppercase:"
+comment = "have a wonderful day"
+puts "Comment: '#{comment}'"
+puts comment.upcase
+
+puts "\n" 
+
+# 2b Capitalise a sentence
+puts "B. Capitalise a Sentence:"
+comment = "have a wonderful day"
+puts "comment: '#{comment}'"
+puts comment.capitalize
+
+puts "\n" 
+
+# 2c Swap the case over 
+puts "C. Swap the case over:"
+comment = "have a WONDERFUL DAY"
+puts "comment: '#{comment}'"
+puts comment.swapcase
+
+puts "\n" 
+
+# 2d Reverse the text 
+puts "D. Reverse the Text"
+comment = "Have A Wonderful Day"
+puts "comment: '#{comment}'"
+puts comment.reverse
